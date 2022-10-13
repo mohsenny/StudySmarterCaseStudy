df0d3947c33dc56d28719e73e7622a8171744028
mohsenny <mohsen.n89@gmail.com>
2022-10-13 08:06:03 +0200

    removed ./scripts

--- scripts/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash 
-
-npm ci
-npm run lint
-npm run test:ci
--- scripts/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash 
-
-# Global Brew Packages
-brew install k6
-
-# Global NPM Packages
-npm install -g @datadog/datadog-ci
