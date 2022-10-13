5ab02057c7359951dac27a901681e7e1e6e946e4
mohsenny <mohsen.n89@gmail.com>
2022-10-13 10:27:01 +0200

    removed setup.js

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,5 @@ module.exports = {
   testRunner: 'jest-circus/runner',
   verbose: true,
   reporters: ['default', 'jest-junit'],
-  testTimeout: 10000,
-  setupFiles: ['./setup.js']
+  testTimeout: 10000
 };
